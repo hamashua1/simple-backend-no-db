27c80e98c12a3e4caeba3a983229d87b722bef01
Joshua Nun <joshuahamashua@gmail.com>
2025-06-05 15:18:55 +0000

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 9001;
 
 app.use(cors()); // Allow all origins for simplicity in development
 app.use(express.json());
